1d156fec9df2dc703cfcb8359186e89671bae651
arroyopablo <pabloandresarroyoparedes@gmail.com>
2020-03-27 16:28:20 -0500

    actulizando carpeta

---
+++ interfaz/DS1-estable/ds1data.sql
@@ -0,0 +1,113 @@
+/*SQL SCRIPT FOR DS1*/
+/*VERSION 1*/
+/* Delete the tables if they already exist */
+drop table if exists Users cascade;
+drop table if exists Sedes cascade;
+drop table if exists Producto cascade;
+drop table if exists Inventario cascade;
+drop table if exists Venta cascade;
+drop table if exists Cotiza cascade;
+drop table if exists Orden cascade;
+drop table if exists vendedoresSede cascade;
+
+/* Create the schema for our tables */
+/*USERS*/
+create table Users (idUser int PRIMARY KEY, first_name varchar(30) NOT NULL, last_name varchar(30), telefono varchar(10), direccion varchar(30), work_position varchar(20) NOT NULL, password varchar(40) NOT NULL, stateUser varchar(8) NOT NULL);
+/*SEDES*/
+create table Sedes (idSedes SERIAL PRIMARY KEY, city varchar(30), address varchar(50) NOT NULL, stateSede varchar(8) NOT NULL, idUser int UNIQUE, FOREIGN KEY (idUser) REFERENCES Users (idUser));
+
+/*TABLA VENDEDOR SEDE*/
+create table vendedoresSede (idSedes int, idUser int UNIQUE, PRIMARY KEY(idSedes, idUser), FOREIGN KEY (idUser) REFERENCES Users (idUser), FOREIGN KEY (idSedes) REFERENCES Sedes (idSedes));
+
+/*PRODUCTO*/
+drop sequence if exists producto_id_seq cascade;
+create sequence producto_id_seq MINVALUE 400 START 401;
+create table Producto (idProducto int PRIMARY KEY DEFAULT nextval('producto_id_seq'), nombre varchar(30), descripcion varchar(50) NOT NULL, color varchar(20) NOT NULL,
+alto numeric, largo numeric, ancho numeric, precio numeric );
+/*INVENTARIO*/
+--INVENTARIO VA A ALMACENAR LOS PRODUCTOS DE CADA SEDE, CADA PRODUCTO TIENE EL ID de la sede a la que corresponde
+create table Inventario (idProducto int, idSedes int, cantidad int, PRIMARY KEY (idProducto, idSedes), FOREIGN KEY (idProducto) REFERENCES Producto (idProducto), FOREIGN KEY (idSedes) REFERENCES Sedes (idSedes));
+/*VENTA*/
+create table Venta (idVenta int PRIMARY KEY, idUser int, precioTotal decimal, fecha date,idsedes int, FOREIGN KEY (idUser) REFERENCES Users (idUser), FOREIGN KEY (idsedes) REFERENCES sedes (idsedes) );
+/*COTIZACION*/
+create table Cotiza (idUser int, idProducto int, fechaCot date, precioCot numeric, PRIMARY KEY (idUser, idProducto), FOREIGN KEY (idUser) REFERENCES Users (idUser), FOREIGN KEY (idProducto) REFERENCES Producto (idProducto));
+/*ORDENES*/
+create table Orden (idOrden SERIAL PRIMARY KEY, cantidad_pedido int, estado_pedido boolean, idProducto int, idSedes int, FOREIGN KEY (idProducto) REFERENCES Producto (idProducto), FOREIGN KEY (idSedes) REFERENCES Sedes (idSedes));
+/* Inserting our data */
+insert into Users values(100, 'admin', 'admin', '5513222','casa 4','admin', 'ADMIN', 'Activo');
+insert into Users values(111, 'Charles', 'Xavier', '1111','casa 1', 'Gerente', 'gorillaz', 'Activo');
+insert into Users values(2488, 'Camilo', 'Sanchez', '2222','casa 2', 'Gerente', '2488', 'Activo');
+insert into Users values(222, 'Elon', 'Musk', '33333','casa 3', 'Jefe de Taller', '123', 'Activo');
+insert into Users values(333, 'David', 'Fincher', '4444','casa 6', 'Jefe de Taller','123', 'Activo');
+insert into Users values(444, 'Itachi', 'Uchiha', '5555','casa 14', 'Vendedor','123', 'Activo');
+insert into Users values(555, 'Lars', 'Von Trier', '7777','casa 10', 'Vendedor','123', 'Inactivo'); --Inactive
+insert into Users values(666, 'Alfred', 'Hitchcock', '63200','casa 30', 'Vendedor','123', 'Activo');
+insert into Users values(777, 'Chritopher', 'Nolan', '63110','casa 80', 'Vendedor','123', 'Activo');
+/*Sedes*/
+insert into Sedes (city, address, stateSede, idUser) values ('Cali', 'Calle 6 casa 7', 'Activo', 222);
+insert into Sedes (city, address, stateSede, idUser) values ('Palmira', 'Calle 18 casa 9', 'Activo', 333);
+/*vendedores sedes*/
+/*La idea es que solo hayan ids de vendedores*/
+insert into vendedoresSede values (1, 444);
+insert into vendedoresSede values (2, 666);
+insert into vendedoresSede values (2, 777);
+
+/*Productos*/
+insert into producto (nombre, descripcion, color, alto, largo, ancho, precio) values ('Super sofa', 'sofa de 3 puestos', 'Cafe Claro', 80, 80, 296, 600000);
+insert into producto (nombre, descripcion, color, alto, largo, ancho, precio) values ('Mega sofa', 'sofa de 2 puestos', 'Blanco', 90, 90, 290, 500000);      
+insert into producto (nombre, descripcion, color, alto, largo, ancho, precio) values ('Sofa Normal', 'sofa de 2 puestos', 'Cafe Oscuro', 80, 80, 296, 200000);      
+insert into producto (nombre, descripcion, color, alto, largo, ancho, precio) values ('Super comedor', 'Comedor para 3 personas', 'Cafe Oscuro', 60, 50, 180, 400000);      
+insert into producto (nombre, descripcion, color, alto, largo, ancho, precio) values ('Mega Comedor', 'comedor para 6 personas', 'Negro', 70, 90, 150, 600000);
+
+/*Inventario*/
+insert into inventario values (401, 1, 30);
+insert into inventario values (402, 1, 30);
+insert into inventario values (403, 1, 15);
+insert into inventario values (404, 1, 5);
+insert into inventario values (405, 1, 1);
+
+insert into inventario values (401, 2, 15);
+insert into inventario values (402, 2, 0);
+insert into inventario values (403, 2, 0);
+insert into inventario values (404, 2, 10);
+insert into inventario values (405, 2, 30);
+--select * from inventario order by idsedes, idproducto;
+
+--PRIMER PROCEDIMIENTO ALMACENADO
+CREATE OR REPLACE function productoNuevo() RETURNS TRIGGER AS $$
+       DECLARE
+       numeroDeSedes integer ;
+       BEGIN
+       IF (TG_OP = 'INSERT') THEN
+              SELECT COUNT(*) INTO numerodeSedes FROM SEDES;
+              FOR i IN 1..numeroDeSedes LOOP
+                     INSERT INTO inventario VALUES(new.idProducto, i, 0);
+              END LOOP;
+       END IF;
+       RETURN NULL;
+       END;
+$$ LANGUAGE plpgsql;
+
+--PRIMER TRIGGER
+CREATE TRIGGER insertarProductoNuevo AFTER INSERT ON producto FOR EACH ROW EXECUTE PROCEDURE productoNuevo();
+
+--SEGUNDO PROCEDIMIENTO ALMACENADO
+
+CREATE OR REPLACE function sedeNueva() RETURNS TRIGGER AS $$
+       DECLARE
+       numeroDeProductos integer;
+       id integer;
+       BEGIN
+       IF (TG_OP = 'INSERT') THEN
+              SELECT count(*) INTO numeroDeProductos FROM PRODUCTO;
+              FOR i IN 1..numeroDeProductos LOOP
+                     select idproducto INTO id from (select *, ROW_NUMBER() OVER () as pos from producto) as foo where foo.pos = i;
+                     INSERT INTO inventario VALUES(id, new.idSedes, 0);
+              END LOOP;
+       END IF;
+       RETURN NULL;
+       END;
+$$ LANGUAGE plpgsql;
+
+--SEGUNDO TRIGGER
+CREATE TRIGGER insertarInventarioNuevo AFTER INSERT ON Sedes FOR EACH ROW EXECUTE PROCEDURE sedeNueva();
---
+++ interfaz/DS1-estable/src/Controladora/Controladora.java
@@ -0,0 +1,313 @@
+package Controladora;
+
+import Modelo.Sedes;
+import Modelo.Users;
+import Modelo.Jefes;
+import Modelo.Orden;
+import Modelo.Producto;
+import Modelo.Vendedor;
+import ModeloDAO.Acceso;
+import ModeloDAO.JefesDAO;
+import ModeloDAO.OrdenDAO;
+import ModeloDAO.ProductoDAO;
+import ModeloDAO.UsersDAO;
+import ModeloDAO.SedesDAO;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import net.sf.jasperreports.engine.JRException;
+
+public class Controladora {
+
+    Acceso access;
+    UsersDAO usersDao;
+    SedesDAO sedesDao;
+    JefesDAO jefesDao;
+    ProductoDAO productoDao;
+    OrdenDAO ordenDao;
+
+    public Controladora() {
+        access = new Acceso();
+        this.usersDao = new UsersDAO(access);
+        this.sedesDao = new SedesDAO(access);
+        this.jefesDao = new JefesDAO(access);
+        this.productoDao = new ProductoDAO(access);
+        this.ordenDao = new OrdenDAO(access);
+    }
+
+    public String login(String user, String pass) {
+        return usersDao.login(user, pass);
+    }
+
+    public Users consultProfile(String userID) {
+        return usersDao.consultProfile(userID);
+    }
+
+    public Jefes consultProfileJefe(String userID) {
+        return jefesDao.consultProfile(userID);
+    }
+
+    public Vendedor consultProfileVENDEDOR(String userID) {
+        //CAMBIAR A VENDEDOR DAO
+        return jefesDao.consultProfileVENDEDOR(userID);
+    }
+
+    public Producto consultProducto(String codigo) {
+        return productoDao.consultProducto(codigo);
+    }
+
+    public Producto consultProductCoincidencia(String valor) {
+
+        return productoDao.consultProductosCoincidencia(valor);
+    }
+
+    public DefaultTableModel mostrarActivos(String sede) {
+        return productoDao.mostrarACTIVOS(sede);
+    }
+
+    public ResultSet maxVenta() throws SQLException {
+        return productoDao.maxIDventa();
+    }
+
+    public void alterComboJefeEditing(JComboBox combo, String[] firstPersonOnCombo) {//NO C
+
+        System.err.println(firstPersonOnCombo);
+        combo.removeAllItems();
+        combo.addItem(firstPersonOnCombo[0] + "," + firstPersonOnCombo[1]);
+        try {
+            ResultSet res = sedesDao.comboOptions();
+
+            while (res.next()) {
+                combo.addItem(res.getString("idUser") + ", " + res.getString("first_name") + " " + res.getString("last_name"));
+            }
+        } catch (Exception e) {
+            System.out.println("---- Problema en la ejecucion.");
+        }
+
+    }
+
+    public void alterComboJefe(JComboBox combo) {
+        combo.removeAllItems();
+        try {
+            ResultSet res = sedesDao.comboOptions();
+            int c = 0;
+            while (res.next()) {
+                c++;
+                combo.addItem(res.getString("idUser") + ", " + res.getString("first_name") + ", " + res.getString("last_name"));
+            }
+            if (c == 0) {
+                combo.addItem("Seleccione");
+            }
+        } catch (Exception e) {
+            System.out.println("---- Problema en la ejecucion.");
+            e.printStackTrace();
+        }
+
+    }
+
+    //Funcion para crear usuarios desde el gerente
+    public boolean createUser(String id, String fname, String lname, String telefono, String direccion, String position, String pass) {
+        Users aU = new Users(id, fname, lname, telefono, direccion, position, pass);
+        if(position.equals("Gerente")){
+            aU.setState("Activo");
+        }else{
+            aU.setState("Inactivo");
+        }
+        return usersDao.createNewUser(aU);
+    }
+
+    //Funcion para crear usuarios vendedor desde la vista del Jefe de taller
+    public boolean createUserVendedor(String id, String fname, String lname, String telefono, String direccion, String pass, String idSede) {
+        Users aU = new Users(id, fname, lname, telefono, direccion, "Vendedor", pass);
+        aU.setState("Activo");
+        return jefesDao.createNewUser(aU, idSede);
+    }
+
+    //Funcion para actualizar usuarios desde el gerente
+    public boolean updateUser(String id, String fname, String lname, String telefono, String direccion, String position) {
+        Users aU = new Users(id, fname, lname, telefono, direccion, position);
+        return usersDao.updateUser(aU);
+    }
+    
+    //Actualizar un gerente desde el Administrador del Sistema
+    public boolean updateGerente(String id, String fname, String lname, String telefono, String direccion, String position) {
+        Users aU = new Users(id, fname, lname, telefono, direccion, position);
+        return usersDao.updateGerente(aU);
+    }
+
+    //Funcion para actualizar usuarios desde el gerente
+    public boolean updateUserVendedor(String id, String fname, String lname, String telefono, String direccion) {
+        Users aU = new Users(id, fname, lname, telefono, direccion, "Vendedor");
+        return jefesDao.updateUserVendedor(aU);
+    }
+
+    //Funcion para actualizar sedes desde el gerente
+    public boolean updateSede(String idSede, String city, String address, String state, String placeid, String jefeActual) {
+        Sedes aS = new Sedes(city, address, state, placeid);
+        aS.setId(idSede);
+        return sedesDao.updateSede(aS, jefeActual);
+    }
+
+    //Funcion para consultar usuarios desde el gerente
+    public ArrayList<String[]> consultUsers() {
+        return usersDao.consultUsers();
+    }
+    
+    public ArrayList<String[]> consultGerente() {
+        return usersDao.consultGerente();
+    }
+    
+    public boolean despedirUsuario(String idUser){
+        return usersDao.despedirUsuario(idUser);
+    }
+    
+    public boolean despedirGerente(String idUser){
+        return usersDao.despedirGerente(idUser);
+    }
+    
+    //Funcion para consultar vendedores desde el Jefe de taller
+    public ArrayList<String[]> consultUsersVendedores(String idSede) {
+        return jefesDao.consultUsersVendedores(idSede);
+    }
+
+    //Funcion para consultar sedes desde el gerente
+    public ArrayList<String[]> consultSedes() {
+        return sedesDao.consultSede();
+    }
+
+    //Funcion para crear sedes desde el gerente
+    public boolean createSede(String ciudad, String direccion, String estado, String jefe) {
+        Sedes sede = new Sedes(ciudad, direccion, estado, jefe);
+        return sedesDao.createNewSede(sede);
+    }
+
+    //Funcion para crear usuarios desde el gerente
+    public boolean crearProducto(String nombre, String descripcion, String color, int alto, int largo, int ancho, int precio) {
+        Producto prod = new Producto("TEMPORAL", nombre, descripcion, color, alto, largo, ancho, precio);
+        return productoDao.crearNuevoProducto(prod);
+    }
+
+    //Funcion para consultar productos desde el gerente
+    public ArrayList<String[]> consultProductos() {
+        return productoDao.consultProductos();
+    }
+    
+    public ArrayList<String[]> consultProductosCoincidencia_tabla(String busqueda) {
+       return productoDao.consultProductosCoincidencia_tabla(busqueda);
+    }
+
+    public void AlterComboVendedoresDisponibles(JComboBox<String> comboVendedoresDisponibles) {
+        comboVendedoresDisponibles.removeAllItems();
+        try {
+            ResultSet res = productoDao.comboOptions();
+            int c = 0;
+            while (res.next()) {
+                c++;
+                comboVendedoresDisponibles.addItem(res.getString("idUser") + ", " + res.getString("first_name") + " " + res.getString("last_name"));
+            }
+            if (c == 0) {
+                comboVendedoresDisponibles.addItem("Seleccione");
+            }
+        } catch (Exception e) {
+            System.out.println("---- Problema en la ejecucion.");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean AsignarUnVendedor(String id, String sedeid) {
+        return jefesDao.asignarUnVendedor(id, sedeid);
+    }
+
+    public ArrayList<String[]> consultProductosDelJefe(String sedeid) {
+        return productoDao.consultProductosDelJefe(sedeid);
+    }
+    public boolean crearOrden(String cantidad, boolean estado, String idProducto, String idSede) {
+        Orden ao = new Orden(cantidad, estado, idProducto, idSede);
+        return ordenDao.crearOrden(ao);
+    }
+
+    public ArrayList<String[]> consultOrders(String sedeid) {
+        return ordenDao.consultOrders(sedeid);
+    }
+
+    public boolean finalizarOrden(String idOrden, String idProd, String sedeid) {
+        return ordenDao.finalizarOrden(idOrden, idProd, sedeid);
+    }
+
+    public boolean updateCantidadResta(String codigo, String cantidad, String idsede) {
+        return productoDao.RestarCantidadProductos(codigo, cantidad, idsede);
+    }
+
+    public boolean updateCantidadSuma(String codigo, String cantidad, String idsede) {
+        return productoDao.SumarCantidadProductos(codigo, cantidad, idsede);
+    }
+
+    public boolean agregarVenta(String idventa, String iduser, String preciototal, String fecha, String idsedes) {
+        return productoDao.agregarVenta(idventa, iduser, preciototal, fecha, idsedes);
+    }
+
+    public DefaultTableModel agregarRegistroVentaVendedor(String iduser) {
+        return productoDao.mostrarRegistrosDeVentaVendedor(iduser);
+    }
+
+    public DefaultTableModel consultarRegistroVentasVendedor(String iduser, String fechaInicio, String fechaFinal) {
+        return productoDao.BuscarRegistrosDeVentaVendedor(iduser, fechaInicio, fechaFinal);
+    }
+
+    public DefaultTableModel agregarRegistroVentaGerente() {
+        return productoDao.mostrarRegistrosDeVentaGerente();
+    }
+
+    public DefaultTableModel consultarRegistroVentasGerente(String fechaInicio, String fechaFinal) {
+        return productoDao.BuscarRegistrosDeVentaGerente(fechaInicio, fechaFinal);
+    }
+
+    public DefaultTableModel agregarRegistroVentaJefeTaller(String idsede) {
+        return productoDao.mostrarRegistrosDeVentaJefeTaller(idsede);
+    }
+
+    public DefaultTableModel consultarRegistroVentasJefeTaller(String idsede, String fechaInicio, String fechaFinal) {
+        return productoDao.BuscarRegistrosDeVentaJefeTaller(idsede, fechaInicio, fechaFinal);
+    }
+
+    public ArrayList<String[]> consultUsersJefes() {
+        return usersDao.consultUsersJefes();
+    }
+    
+    public ArrayList<String[]> consultUsersVendedores() {
+        return usersDao.consultUsersVendedores();
+    }
+
+    public ArrayList<String[]> consultUsersCoincidencia_tabla(String busqueda) {
+        return usersDao.consultUsersCoincidencia_tabla(busqueda);
+    }
+
+    public ArrayList<String[]> consultSedesCoincidencia_tabla(String busqueda) {
+        return sedesDao.consultSedesCoincidencia_tabla(busqueda);
+    }
+
+    public ArrayList<String[]> consultProductosCoincidenciaDelJefe(String idSede, String busqueda) {
+        return productoDao.consulProductosCoincidenciaDelJefe(idSede, busqueda);
+    }
+
+    public ArrayList<String[]> consultUsersVendedoresCoincidencia(String idSede, String busqueda) {
+        return jefesDao.consultUsersVendedoresCoincidencia(idSede, busqueda);
+    }
+
+    public void generarReporte(String finicio, String ffinal) throws JRException {
+        productoDao.generarReporte(finicio, ffinal);
+    }
+
+    public ArrayList<String[]> consultOrdenesCoincidencia(String idSede, String busqueda) {
+        return ordenDao.consultOrdenesCoincidencia(idSede, busqueda);
+    }
+
+    public void generarReporteVentasJefeDeTaller(String sede, String fechaInicio, String fechaFinal) throws JRException {
+        productoDao.generarReporteVentasJefeDeTaller(sede, fechaInicio, fechaFinal);
+    }
+
+
+}
